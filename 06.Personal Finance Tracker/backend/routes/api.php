8448574cd9df68715a34421d0d8de86af1efdc95
dirgaastya <astya444@gmail.com>
2025-07-19 11:21:39 +0700

    feat (api) : add endpoint for category and transaction feature

--- 06.Personal Finance Tracker/backend/routes/api.php
+++ 06.Personal Finance Tracker/backend/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+    Route::prefix('category')->group(function () {
+        Route::get('get-all-categories',[CategoriesController::class,'getAllCategories']);
+    });
+    Route::prefix('transaction')->group(function () {
+        Route::post('get-transactions',[TransactionController::class,'getTransactions']);
+    });
